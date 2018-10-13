96842363e3465ee2892220d725d1a5b89859b944
Roman Wehmhöner <r.wehmhoner@gmail.com>
2018-10-13 17:51:15 +0200

    Add project files.

---
+++ Electron/Run.js
@@ -0,0 +1,15 @@
+﻿const exec = require('child_process').exec;
+const child = exec('npm start',
+    (error, stdout, stderr) =>
+    {
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        if (error !== null) {
+            console.log(`exec error: ${error}`);
+        }
+    });
+
+child.on("close", (_c, _s) =>
+{
+    process.exit();
+});
