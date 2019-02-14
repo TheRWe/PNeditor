27dc07ac6c2170bd731e5bd057bc49091d0c0539
Roman Wehmhöner <r.wehmhoner@gmail.com>
2019-02-14 17:13:06 +0100

    settings

---
+++ Electron/Editor/settings.ts
@@ -0,0 +1,23 @@
+﻿import * as Sett from "./EditorHelpers/SettingsInterface"
+// todo: vymyslet rozumné propojení pro případné uživatelské nastavení nebo editor
+export const sett: Sett.Settings =
+{
+    modes: {
+        main: ["default", "arc-make"],
+        // todo: pokročilé dependencies - and/or a podobně
+        // todo: second name
+        // todo: initial value
+        // todo: save state
+        // todo: multistate toggle ???
+        toggles: [
+        "Run",
+        {
+            name: "Delete",
+            dependencies: {
+                toggles: [{ name: "Run", value: false }]
+            }
+        },]
+    },
+    actions: [
+    ]
+}
